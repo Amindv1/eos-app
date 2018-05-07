2e050f398373a01422d75f73a9870cba821d9a6d
Amin <amind1@vt.edu>
2018-05-07 12:00:59 -0400

    added apollo cache and read/write queries

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+// import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import Eos from 'eosjs';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import ExpandRow from './ExpandRow';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-client';
+import { withApollo, ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
 import './App.css';
 import './react-bootstrap-table.css';
+import { makeExecutableSchema } from 'graphql-tools';
+import fetch from 'node-fetch';
+import _ from 'lodash';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.eos = Eos.Testnet();
+  constructor(props) {
+    super(props);
+    this.eos = Eos.Localnet({ httpEndpoint: "http://localhost:8888"});
+    this.getBlocks = this.getBlocks.bind(this);
   }
 
   getBlocks() {
+
+    this.eos.getInfo({}).then(result => {
+      let lastBlock = result.head_block_num;
+      for (let i = 0; i < 10; i++) {
+        let block_id = lastBlock - i;
+        this.eos.getBlock({block_num_or_id: block_id}).then(result => {
+          console.log(result);
+          this.props.client.writeQuery({
+            query: gql`
+            query writeBlock {
+              block(raw: String, count: String, timestamp: String) {
+                timestamp
+                raw
+                count
+              }
+            }`,
+            data: {id: result.id, block: {
+              timestamp: result.timestamp,
+              raw: result,
+              count: result.input_transactions.length,
+            }}
+          });
+
+          const data = this.props.client.readQuery({
+            query: gql`
+            query readBlock {
+              block(raw: String, count: String, timestamp: String) {
+                timestamp
+                raw
+                count
+              }
+            }`
+          });
+          console.log(data);
+        });
+      }
+    });
   }
   
   render() {
@@ -72,11 +117,11 @@ class App extends Component {
           <ExpandRow data={data}/>
         </div>
         <p className="App-intro">
-          <button type="button" onClick="getBlocks()"> Load blocks</button>
+          <button type="button" onClick={this.getBlocks}> Load blocks</button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default withApollo(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { withApollo, ApolloProvider, Query } from "react-apollo";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-client';
+import { makeExecutableSchema } from 'graphql-tools';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache({addTypename: false});
+const httpLink = new HttpLink({ uri: 'http://localhost:8888/v1/chain'});
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+ReactDOM.render(
+<ApolloProvider client={client}> 
+<App client={client}/> 
+</ApolloProvider>, 
+document.getElementById('root'));
 registerServiceWorker();
